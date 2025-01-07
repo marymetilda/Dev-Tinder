009bbf5da0d277a9e8ec6a295ec921777d937f3b
marymetilda <metildaphy@gmail.com>
2025-01-07 10:45:54 +0530

    add cors library

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
-const app = express();
-
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const app = express();
 // const ALLOWED_UPDATES = ["firstName", "lastName", "age", "gender", "skills"];
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
